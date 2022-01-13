a39972883704fe1103ee75346dbe22cbb5c5013e
kocherizhka <ulyakleeva@mail.ru>
2022-01-13 17:43:34 +0300

    добавила создание матрицы заданного размера с заданными значениями

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 def input1():
     return int(input()), int(input())
 
+def cube(t, e):
+    q = []
+    for i in range(t):
+        q.append([e] * t)
+    return q
+
 if __name__ == '__main__':
     pass
