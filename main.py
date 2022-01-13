d857cd1b5acdc39acf30ac2971662727036cc63e
vezype <vezype@gmail.com>
2022-01-13 17:42:55 +0300

    Добавлена функция вычисления факториала.

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 def input1():
     return int(input()), int(input())
 
+
+def factorial(number: int) -> int:
+    if not number:
+        return 1
+    else:
+        return factorial(number - 1) * number
+
+
 if __name__ == '__main__':
     pass
